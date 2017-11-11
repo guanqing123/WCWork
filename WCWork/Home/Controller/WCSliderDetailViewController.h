2404fee168bb9291640764c0eb245472350177ed
guanqing123 <guanqinghz@163.com>
2017-11-11 16:48:05 +0800

    同上

---
+++ WCWork/Home/Controller/WCSliderDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  WCSliderDetailViewController.h
+//  WCWork
+//
+//  Created by information on 2017/10/30.
+//  Copyright © 2017年 hongyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <WebKit/WebKit.h>
+
+@interface WCSliderDetailViewController : UIViewController
+
+- (instancetype)initWithUrlPath:(NSString *)urlPath;
+
+@end
